795c0e8e76bb6a81b56b9da6998d032b183c7de8
Amory Meltzer <Amorymeltzer@gmail.com>
2015-08-18 11:39:17 -0400

    Tidy

--- sysopHindex.pl
+++ sysopHindex.pl
@@ -8,84 +8,80 @@ use warnings;
 use diagnostics;
 
 unless (@ARGV == 2)
-{
+  {
     print "Usage: $0 <then.txt> <now.txt>\n";
     exit;
-}
+  }
 
 my %oldAdmin;
 my %newAdmin;
-my %final; # holds the difference
-my $count = 0; # global
+my %final;			# holds the difference
+my $count = 0;			# global
 
 # Populate hashes from relevant data source
-open (my $old, "<", "$ARGV[0]") or die $!;
-while (<$old>)
-{
-    my @array = basicFormat($_);
-    next if $array[0] =~ m/^User$|^Totals$/o;
-    next if $array[0] =~ m/[bB]ot$/o;
-    $oldAdmin{$array[0]} = $array[-1];
+open my $old, '<', "$ARGV[0]" or die $!;
+while (<$old>) {
+  my @array = basicFormat($_);
+  next if $array[0] =~ m/^User$|^Totals$/o;
+  next if $array[0] =~ m/[bB]ot$/o;
+  $oldAdmin{$array[0]} = $array[-1];
 }
-close ($old);
+close $old or die $!;
 
-open (my $new, "<$ARGV[1]") or die $!;
-while (<$new>)
-{
-    my @array = basicFormat($_);
-    next if $array[0] =~ m/^User$|^Totals$/o;
-    next if $array[0] =~ m/[bB]ot$/o;
-    $newAdmin{$array[0]} = $array[-1];
+open my $new, '<', "$ARGV[1]" or die $!;
+while (<$new>) {
+  my @array = basicFormat($_);
+  next if $array[0] =~ m/^User$|^Totals$/o;
+  next if $array[0] =~ m/[bB]ot$/o;
+  $newAdmin{$array[0]} = $array[-1];
 }
-close ($new);
+close $new or die $!;
 
 
 # The difference for each individual sysop aka total log actions for that time period
-foreach my $key (sort keys %newAdmin)
-{
-#    next unless exists $oldAdmin{$key};
-    my $act = $newAdmin{$key};
-    if (exists $oldAdmin{$key})
-    {
-	$act = $newAdmin{$key}-$oldAdmin{$key};
-    }
+foreach my $key (sort keys %newAdmin) {
+  #    next unless exists $oldAdmin{$key};
+  my $act = $newAdmin{$key};
+  if (exists $oldAdmin{$key}) {
+    $act = $newAdmin{$key}-$oldAdmin{$key};
+  }
 
-# Alternate to above, maybe slower since it ALWAYS does three actions?
-#    my $act = $oldAdmin{$key} // 0;
-#    $act = $newAdmin{$key} - $act;
+  # Alternate to above, maybe slower since it ALWAYS does three actions?
+  #    my $act = $oldAdmin{$key} // 0;
+  #    $act = $newAdmin{$key} - $act;
 
 
-#    print "$act\t$key\n"; # log, sysop for sorting
-    $final{$key} = $act;
+  #    print "$act\t$key\n"; # log, sysop for sorting
+  $final{$key} = $act;
 
 
-# Debug, to try to find user renames
-# Can also just reverse dates, anyone who shows got renamed
-#    elsif ($newAdmin{$key} > 50)
-#    {
-#	print "$key\t$newAdmin{$key}\n";
-#    }
+  # Debug, to try to find user renames
+  # Can also just reverse dates, anyone who shows got renamed
+  #    elsif ($newAdmin{$key} > 50)
+  #    {
+  #	print "$key\t$newAdmin{$key}\n";
+  #    }
 }
 
 
 # Calculate the score
 foreach my $key (sort {$final{$b} <=> $final{$a} || $a cmp $b} (keys %final)) #high->low, a->z
-{
+  {
     next if $final{$key} == 0;
     last if $count > $final{$key};
     $count++;
-#    print "$final{$key}\t$key\t$count\n"; # sorted actions until h-index
-}
+    #    print "$final{$key}\t$key\t$count\n"; # sorted actions until h-index
+  }
 
 #print "Sysop H-index:\t$count\n";
 print "$count\n";
 
 
 sub basicFormat
-{
+  {
     my ($line) = @_;
     chomp $line;
-#    $line =~ s/\"//g; #names with commas are exported in quotes by excel
+    #    $line =~ s/\"//g; #names with commas are exported in quotes by excel
     my @array = split(/\t/,$line);
     return @array;
-}
+  }
