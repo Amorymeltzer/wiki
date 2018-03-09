e26bfaba5ebdb6bd7e043f99df1ca7589e709e81
Amory Meltzer <Amorymeltzer@gmail.com>
2018-03-09 15:05:58 -0500

    Keep track of latest downloaded

--- getData.pl
+++ getData.pl
@@ -1,16 +1,22 @@
 #!/usr/bin/env perl
 # getData.pl by Amory Meltzer
 # Bulk download data monthly data from [[User:JamesR/AdminStats]]
-## Starts September 2008 (could do August, but meh)
+## Data starts September 2008 (could do August, but meh)
 ## Should check timestamps to confirm data quality
-## Make sure don't surpass this month of this year?
 
 
 use strict;
 use warnings;
 use diagnostics;
+use English qw( -no_match_vars);
 
-my $startYear = 2008;
+if (@ARGV != 1) {
+  print "Usage: $PROGRAM_NAME <getData.pl> <latest>\n";
+  exit;
+}
+
+# Earliest reliable data is from Semptember 2008, don't use anything older
+my ($startYear,$startMonth) = (2008,8);
 # 0-indexed
 my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)=localtime;
 # Not anymore!
@@ -18,12 +24,21 @@ $mon++;
 # 1900-indexed
 my $endYear = $year+1900;
 
+open my $latest, '<', "$ARGV[0]" or croak $ERRNO;
+while (<$latest>) {
+  chomp;
+  if ($_) {
+    ($startYear,$startMonth) = split /-/;
+  }
+}
+close $latest or croak $ERRNO;
+
 for my $year (($startYear..$endYear)) {
   for my $month (1..12) {
-    next if ($year == $startYear && $month < 9);
+    next if ($year == $startYear && $month <= $startMonth);
     next if ($year == $endYear && $month > $mon);
-    $month = sprintf("%02d", $month);
-    my $date = $year.'-'.$month.'-01';
+    $month = sprintf '%02d', $month ;
+    my $date = $year.q{-}.$month.q{-}.'01';
     print "$date\n";
   }
 }
