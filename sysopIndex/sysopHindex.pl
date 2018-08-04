5742a148f309b298a4891ebf9e99a16f908af9c3
Amory Meltzer <Amorymeltzer@gmail.com>
2018-08-04 14:20:05 -0400

    Rough rewrite of sysopHindex.pl for new style
    Only takes one file now

--- sysopIndex/sysopHindex.pl
+++ sysopIndex/sysopHindex.pl
@@ -8,78 +8,70 @@
 ## Probably need more efficient index calculation
 
 
-
 use strict;
 use warnings;
 use diagnostics;
 use English qw( -no_match_vars);
 
-if (@ARGV != 2) {
-  print "Usage: $PROGRAM_NAME <then.txt> <now.txt>\n";
+if (@ARGV < 1) {
+  print "Usage: $PROGRAM_NAME month.csv <month2.csv month3.csv ...>\n";
   exit;
 }
 
 my %oldAdmin;
 my %newAdmin;
-my %final;			# holds the difference
 my $count = 0;			# global
 
 # Populate hashes from relevant data source
 parseFile($ARGV[0],\%oldAdmin);
-parseFile($ARGV[1],\%newAdmin);
 
+foreach my $num (1..scalar @ARGV - 1) {
+  print "$num\t$ARGV[$num]\n";
+  parseFile($ARGV[$num],\%newAdmin);
 
-# The difference for each individual sysop aka total log actions for that time period
-foreach my $key (sort keys %newAdmin) {
-  #    next unless exists $oldAdmin{$key};
-  my $act = $newAdmin{$key};
-  if (exists $oldAdmin{$key}) {
-    $act = $newAdmin{$key}-$oldAdmin{$key};
+  # Add latest data
+  foreach my $key (keys %newAdmin) {
+    if ($oldAdmin{$key}) {
+      $oldAdmin{$key} += $newAdmin{$key};
+    } else {
+      $oldAdmin{$key} = $newAdmin{$key};
+    }
   }
-
-  # Alternate to above, maybe slower since it ALWAYS does three actions?
-  #    my $act = $oldAdmin{$key} // 0;
-  #    $act = $newAdmin{$key} - $act;
-
-
-  #    print "$act\t$key\n"; # log, sysop for sorting
-  $final{$key} = $act;
-
-
-  # Debug, to try to find user renames
-  # Can also just reverse dates, anyone who shows got renamed
-  #    elsif ($newAdmin{$key} > 50)
-  #    {
-  #	print "$key\t$newAdmin{$key}\n";
-  #    }
 }
+
+# Debug, to try to find user renames
+# Can also just reverse dates, anyone who shows got renamed
+#    elsif ($newAdmin{$key} > 50)
+#    {
+#	print "$key\t$newAdmin{$key}\n";
+#    }
 
 
 # Calculate the score
-foreach my $key (sort {$final{$b} <=> $final{$a} || $a cmp $b} (keys %final)) #high->low, a->z
+foreach my $key (sort {$oldAdmin{$b} <=> $oldAdmin{$a} || $a cmp $b} (keys %oldAdmin)) #high->low, a->z
   {
-    next if $final{$key} == 0;
-    last if $count > $final{$key};
+    next if $oldAdmin{$key} == 0;
+    last if $count > $oldAdmin{$key};
     $count++;
-    #    print "$final{$key}\t$key\t$count\n"; # sorted actions until h-index
+    print "$oldAdmin{$key}\t$key\t$count\n"; # sorted actions until h-index
   }
 
-#print "Sysop H-index:\t$count\n";
+#  print "Sysop H-index:\t$count\n";
 print "$count\n";
 
 
 sub parseFile
-    {
-      my ($file,$hashRef) = @_;
-      open my $data, '<', "$file" or die $ERRNO;
-      while (<$data>) {
-	# Column names not needed, but nice to have in the input files
-	next if $NR == 1;
-	chomp;
-	my @array = split /,/;
-	next if $array[0] =~ m/[bB]ot$/o;
-	${$hashRef}{$array[0]} = $array[-1];
-      }
-      close $data or die $ERRNO;
-      return;
+  {
+    my ($file,$hashRef) = @_;
+    open my $data, '<', "$file" or die $ERRNO;
+    while (<$data>) {
+      # Column names not needed, but nice to have in the input files
+      next if $NR == 1;
+      chomp;
+      my @array = split /,/;
+      next if $array[0] =~ m/[bB]ot$/o;
+      ${$hashRef}{$array[0]} = $array[-1];
     }
+    close $data or die $ERRNO;
+    return;
+  }
