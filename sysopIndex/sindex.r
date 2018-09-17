fd6749589e3bbac0793cdcbf6c367d35166ffa2a
Amory Meltzer <Amorymeltzer@gmail.com>
2018-09-17 17:52:13 -0400

    Graph two charts, one with and one without totals

--- sysopIndex/sindex.r
+++ sysopIndex/sindex.r
@@ -9,19 +9,22 @@ library(scales)
 
 # Import data
 args=commandArgs(trailingOnly = TRUE)
-#args=c('sindex.csv','monthly')
-dm=read.csv(args[1],header=T)
-# Reorder for ggplot
-dm <- dm[,c(1,2,4,3,5)]
-# Format dates and values, will be useful for scaling x-axis
-dm[[1]] <- as.Date(as.yearmon(dm[[1]]))
-dm$Total = dm$Total/1500
-dm$Total.bot = dm$Total.bot/1500
+#args=c('sindex-monthly.csv','monthly')
+#args=c('sindex-roll3.csv','rolling (3mos)')
+dmt=read.csv(args[1],header=T)
+# Format dates, useful for scaling x-axis
+dmt[[1]] <- as.Date(as.yearmon(dmt[[1]]))
+# Reorder for ggplot, make total-free
+dmt <- dmt[,c(1,2,4,3,5)]
+dm <- dmt[,c(1,2,3)]
+# Format values, useful for scaling y-axis
+dmt$Total = dmt$Total/1500
+dmt$Total.bot = dmt$Total.bot/1500
 # Two lines by melting
 dm_melt = melt(dm, id = names(dm)[1])
-# For line aesthetics
-dm_melt$type <- ifelse(grepl("Total",dm_melt$variable), "total", "index")
-
+dmt_melt = melt(dmt, id = names(dmt)[1])
+# For line aesthetics?  Dash geom_smooth
+#dm_melt$type <- ifelse(grepl("Total",dm_melt$variable), "total", "index")
 
 # Theme modified from Max Woolf
 # https://minimaxir.com/2015/02/ggplot-tutorial/
@@ -82,5 +85,26 @@ plot3 <- ggplot(dm_melt, aes_string(x = names(dm_melt[1]), y = names(dm_melt[3])
 #options(warn = -1)
 #plot3 <- plot3+scale_y_continuous(sec.axis = sec_axis(~.*1500, name = "Total actions", breaks=derive(),labels=comma))
 #options(warn = 0)
-plot3+geom_smooth(se=FALSE, method=loess)
-ggsave(paste("S-index (",args[2],").png", sep=''), plot3, width=4.92, height=3)
+plot3+geom_smooth(se=FALSE, method=loess, size=0.75, show.legend=F)
+
+
+buildPlot <- function(mf,tot)
+{
+  p<-ggplot(mf, aes_string(x = names(mf[1]), y = names(mf[3]), colour = names(mf[2]), group = names(mf[2]))) + geom_line(aes(linetype=variable)) +
+    scale_x_date(date_labels = "%b %y",breaks=pretty_breaks(6)) +
+    scale_y_continuous(breaks=pretty_breaks(6)) +
+    labs(title=paste("Sysop index",args[2], sep=' - '),
+         x=names(mf)[1],
+         y="S-index",
+         caption="User:Amorymeltzer") +
+    scale_linetype_manual(values=c("solid", "solid", "dotted", "dashed")) +
+    modfte_theme() + scale_colour_manual(values=c('#4DAF4A','#984EA3','grey75','grey75'))
+  #options(warn = -1)
+  #plot3 <- plot3+scale_y_continuous(sec.axis = sec_axis(~.*1500, name = "Total actions", breaks=derive(),labels=comma))
+  #options(warn = 0)
+#  p+geom_smooth(se=FALSE, method=loess, size=0.75, show.legend=F)
+  ggsave(paste("S-index (",args[2],tot,").png", sep=''), p, width=4.92, height=3)
+}
+buildPlot(dm_melt,'')
+buildPlot(dmt_melt,' - total')
+#ggsave(paste("S-index (",args[2],").png", sep=''), plot3, width=4.92, height=3)
