9f4e31da75582bc4ba675980623671ea0ee72d23
Amory Meltzer <Amorymeltzer@gmail.com>
2018-09-18 16:54:22 -0400

    Option in shim to just recalculate, skip trying to download data

--- sysopIndex/sIndex.sh
+++ sysopIndex/sIndex.sh
@@ -23,12 +23,14 @@
 function get_help {
     cat <<END_HELP
 Usage: $(basename $0)
-  -h		this help
+  -r		Recalculate only (i.e. don't try and download new data)
+  -h		This help
 END_HELP
 }
 
-while getopts 'hH' opt; do
+while getopts 'rhH' opt; do
     case $opt in
+	r) recalc_only='1';;
 	h|H) get_help $0
 	     exit 0;;
 	\?) printf "Invalid option: -"$opt", try $0 -h\n" >&2
@@ -44,78 +46,85 @@ function dienice() {
     exit 1
 }
 
-# Keep track of latest data grab
-latest="latest"
-
-if [[ -a "$latest" ]]; then
-    latest=$(cat $latest)
-else
-    latest='initialize';
-fi
-
-# All missing data
-dates=$(perl getDates.pl "$latest")
-if [ -z "$dates" ]; then
-    dienice "No more dates to process!"
-fi
-
 # Directories
 rawD="rawData"
 csvD="csvData"
-# URL
-#/enWiki/2018-03-01/2018-03-31' -o 2018-03-01.2018-03-31.xtool
-urlBase="https://xtools.wmflabs.org/adminstats/enWiki/"
 
-# Bulk download data monthly data from [[User:JamesR/AdminStats]]
-for date in $dates
-do
-    mon=$(echo ${date:0:7})
-    raw=$rawD/$mon.'html'
-    echo "Downloading $date..."
-    url="$urlBase$date"
-    echo $url
-    curl -d '' "$url" -o $raw.tmp
+function process_data() {
+    # Keep track of latest data grab
+    latest="latest"
 
-    # Remove variable/easter egg content
-    lines=$(wc -l $raw.tmp |xargs|cut -f 1 -d ' ')
-    ((lines -= 50))
-    head -n $lines $raw.tmp > $raw
-    rm $raw.tmp
-    md5 -r $raw >> "md5raw.txt"
-
-    timestamp=$(grep -A 2 "Ending date" $raw |tail -n 1|xargs)
-    timestamp=$(echo ${timestamp:0:7})
-
-    if [ "$timestamp" != "$mon" ]; then
-	echo
-	echo "WARNING: TIMESTAMP FOR $date seems erroneous!"
-	exit
+    if [[ -a "$latest" ]]; then
+	latest=$(cat $latest)
+    else
+	latest='initialize';
     fi
 
-    csv=$csvD/$mon.'csv'
-    perl table2csv.pl $raw > $csv
-    md5 -r $csv >> "md5csv.txt"
-done
-rawDups=$(sort "md5raw.txt" | uniq -d)
-csvDups=$(sort "md5csv.txt" | uniq -d)
-echo
+    # All missing data
+    dates=$(perl getDates.pl "$latest")
+    if [ -z "$dates" ]; then
+	dienice "No more dates to process!"
+    fi
 
-if [[ -n $rawDups ]]; then
-    echo "Duplicate raw data files found"
-    for dup in $rawDups
-    do
-	echo $dup
-    done
-    dienice "You should investigate manually"
-fi
+    # URL
+    #/enWiki/2018-03-01/2018-03-31' -o 2018-03-01.2018-03-31.xtool
+    urlBase="https://xtools.wmflabs.org/adminstats/enWiki/"
 
-if [[ -n $csvDups ]]; then
-    echo "Duplicate csv data files found"
-    for dup in $csvDups
+    # Bulk download data monthly data from [[User:JamesR/AdminStats]]
+    for date in $dates
     do
-	echo $dup
+	mon=$(echo ${date:0:7})
+	raw=$rawD/$mon.'html'
+	echo "Downloading $date..."
+	url="$urlBase$date"
+	echo $url
+	curl -d '' "$url" -o $raw.tmp
+
+	# Remove variable/easter egg content
+	lines=$(wc -l $raw.tmp |xargs|cut -f 1 -d ' ')
+	((lines -= 50))
+	head -n $lines $raw.tmp > $raw
+	rm $raw.tmp
+	md5 -r $raw >> "md5raw.txt"
+
+	timestamp=$(grep -A 2 "Ending date" $raw |tail -n 1|xargs)
+	timestamp=$(echo ${timestamp:0:7})
+
+	if [ "$timestamp" != "$mon" ]; then
+	    echo
+	    echo "WARNING: TIMESTAMP FOR $date seems erroneous!"
+	    exit
+	fi
+
+	csv=$csvD/$mon.'csv'
+	perl table2csv.pl $raw > $csv
+	md5 -r $csv >> "md5csv.txt"
     done
-    dienice "You should investigate manually"
+    rawDups=$(sort "md5raw.txt" | uniq -d)
+    csvDups=$(sort "md5csv.txt" | uniq -d)
+    echo
+
+    if [[ -n $rawDups ]]; then
+	echo "Duplicate raw data files found"
+	for dup in $rawDups
+	do
+	    echo $dup
+	done
+	dienice "You should investigate manually"
+    fi
+
+    if [[ -n $csvDups ]]; then
+	echo "Duplicate csv data files found"
+	for dup in $csvDups
+	do
+	    echo $dup
+	done
+	dienice "You should investigate manually"
+    fi
+}
+
+if [[ -z $recalc_only ]]; then
+    process_data
 fi
 
 # Rewrite calcH to take list of files, pass directory name from here
