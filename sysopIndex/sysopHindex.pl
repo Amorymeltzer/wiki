a3bcdabea0880d69d7a00a0bcb45c03316f873d5
Amory Meltzer <Amorymeltzer@gmail.com>
2018-09-19 12:30:09 -0400

    Subroutine code

--- sysopIndex/sysopHindex.pl
+++ sysopIndex/sysopHindex.pl
@@ -5,9 +5,6 @@
 # Approximate the h-index/Eddington number for admin actions using
 # XTools AdminStats
 
-## Probably need more efficient index calculation
-
-
 use strict;
 use warnings;
 use diagnostics;
@@ -18,70 +15,63 @@ if (@ARGV < 1) {
   exit;
 }
 
-my %oldAdmin;
-my %oldAdminBot;
-my ($count,$total) = (0,0);
-my ($countBot,$totalBot) = (0,0);
-
-# Populate hashes from relevant data source
-parseFile($ARGV[0],\%oldAdmin,\%oldAdminBot);
-
+# Populate initial hashes
+my (%oldAdmin,%oldAdminBot);
+buildArray($ARGV[0],\%oldAdmin,\%oldAdminBot);
+# Combine data from each subsequent file
 foreach my $num (1..scalar @ARGV - 1) {
   my (%newAdmin,%newAdminBot);
-  parseFile($ARGV[$num],\%newAdmin,\%newAdminBot);
-
-  # Add latest data
-  foreach my $key (keys %newAdmin) {
-    if ($oldAdmin{$key}) {
-      $oldAdmin{$key} += $newAdmin{$key};
-    } else {
-      $oldAdmin{$key} = $newAdmin{$key};
-    }
-  }
-  foreach my $key (keys %newAdminBot) {
-    if ($oldAdminBot{$key}) {
-      $oldAdminBot{$key} += $newAdminBot{$key};
-    } else {
-      $oldAdminBot{$key} = $newAdminBot{$key};
-    }
-  }
+  buildArray($ARGV[$num],\%newAdmin,\%newAdminBot);
+  mergeData(\%oldAdmin,\%newAdmin);
+  mergeData(\%oldAdminBot,\%newAdminBot);
 }
 
-
 # Calculate the score
-foreach my $key (sort {$oldAdmin{$b} <=> $oldAdmin{$a} || $a cmp $b} (keys %oldAdmin)) #high->low, a->z
-  {
-    next if $oldAdmin{$key} == 0;
-    $total += $oldAdmin{$key};
-    if ($count <= $oldAdmin{$key}) {
-      $count++;
-    }
-  }
-foreach my $key (sort {$oldAdminBot{$b} <=> $oldAdminBot{$a} || $a cmp $b} (keys %oldAdminBot)) #high->low, a->z
-  {
-    next if $oldAdminBot{$key} == 0;
-    $totalBot += $oldAdminBot{$key};
-    if ($countBot <= $oldAdminBot{$key}) {
-      $countBot++;
-    }
-  }
-
+my ($count,$total) = calcIndex(\%oldAdmin);
+my ($countBot,$totalBot) = calcIndex(\%oldAdminBot);
 print "$count,$total,$countBot,$totalBot\n";
 
 
-sub parseFile
-  {
-    my ($file,$hashRef,$hashRefBot) = @_;
-    open my $data, '<', "$file" or die $ERRNO;
-    while (<$data>) {
-      # Column names not needed, but nice to have in the input files
-      next if $NR == 1;
-      chomp;
-      my @array = split /,/;
-      ${$hashRefBot}{$array[0]} = $array[-1];
-      next if ($array[0] =~ m/bot\b/io);
-      ${$hashRef}{$array[0]} = $array[-1];
-    }
-    close $data or die $ERRNO;
-    return;
+# Subroutines
+sub buildArray {
+  my ($file,$hashRef,$hashRefBot) = @_;
+  open my $data, '<', "$file" or die $ERRNO;
+  while (<$data>) {
+    # Column names not needed, but nice to have in the input files
+    next if $NR == 1;
+    chomp;
+    my @array = split /,/;
+    ${$hashRefBot}{$array[0]} = $array[-1];
+    next if ($array[0] =~ m/bot\b/io);
+    ${$hashRef}{$array[0]} = $array[-1];
   }
+  close $data or die $ERRNO;
+  return;
+}
+
+sub mergeData {
+  my ($hashRef,$hashRefNew) = @_;
+  foreach my $key (keys %{$hashRefNew}) {
+    if (${$hashRef}{$key}) {
+      ${$hashRef}{$key} += ${$hashRefNew}{$key};
+    } else {
+      ${$hashRef}{$key} = ${$hashRefNew}{$key};
+    }
+  }
+  return;
+}
+
+sub calcIndex {
+  my ($hashRef) = @_;
+  #my ($index,$tot) = (0,0);
+  my @indTot = (0,0);
+  #high->low, a->z
+  foreach my $key (sort {${$hashRef}{$b} <=> ${$hashRef}{$a} || $a cmp $b} (keys %{$hashRef})) {
+    next if ${$hashRef}{$key} == 0;
+    $indTot[1] += ${$hashRef}{$key};
+    if ($indTot[0] <= ${$hashRef}{$key}) {
+      $indTot[0]++;
+    }
+  }
+  return @indTot;
+}
