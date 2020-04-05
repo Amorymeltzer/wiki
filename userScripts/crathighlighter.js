f82e09162ea263908d1677a87a2ca1d16d230c7e
Amory Meltzer <Amorymeltzer@gmail.com>
2020-04-05 07:38:06 -0400

    crathighlighter.js: Slightly tighter looping, less repetition, no unnecessary class adding

--- userScripts/crathighlighter.js
+++ userScripts/crathighlighter.js
@@ -27,19 +27,21 @@ var highlight_order = window.highlight_order || ['arbcom', 'bureaucrat', 'oversi
 var main = function(data) {
 	var ADMINHIGHLIGHT_EXTLINKS = window.ADMINHIGHLIGHT_EXTLINKS || false;
 	var ADMINHIGHLIGHT_NAMESPACES = [-1,2,3];
-	var classLookup = {};
-	for (var perm of highlight_order) {
-		classLookup[perm] = 'userhighlighter_' + perm;
-	}
+	var classDefs = {
+		arbcom: '888888',
+		bureaucrat: '5588FF',
+		oversight: 'DD66DD',
+		checkuser: 'FFFF00',
+		'interface-admin': '66DD66',
+		sysop: '00FFFF',
+		steward: 'FF9933'
+	};
 
 	mw.loader.using(['mediawiki.util','mediawiki.Uri', 'mediawiki.Title'], function() {
-		mw.util.addCSS(".userhighlighter_arbcom {background-color: #888888}");
-		mw.util.addCSS(".userhighlighter_bureaucrat {background-color: #5588FF}");
-		mw.util.addCSS(".userhighlighter_oversight {background-color: #DD66DD}");
-		mw.util.addCSS(".userhighlighter_checkuser {background-color: #FFFF00}");
-		mw.util.addCSS(".userhighlighter_interface-admin {background-color: #66DD66}");
-		mw.util.addCSS(".userhighlighter_sysop {background-color: #00FFFF}");
-		mw.util.addCSS(".userhighlighter_steward {background-color: #FF9933}");
+		for (var perm of highlight_order) {
+			mw.util.addCSS(".userhighlighter_" + perm + " {background-color: #" + classDefs[perm] + "}");
+		}
+
 		$('#mw-content-text a').each(function(index,linkraw){
 			try {
 				var link = $(linkraw);
@@ -58,7 +60,7 @@ var main = function(data) {
 						if (mwtitle.getNamespaceId() === -1) user = user.replace('Contributions/','');
 						$.each(highlight_order, function(_ix, ug) {
 							if(data[ug][user] === 1) {
-								link.attr("class", classLookup[ug]);
+								link.attr("class", "userhighlighter_" + ug);
 								return false;
 							}
 						});
