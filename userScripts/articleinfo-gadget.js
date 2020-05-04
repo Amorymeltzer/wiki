87e8a50db150720b64c9ed46fa4a57aeb2c32dcd
Amory Meltzer <Amorymeltzer@gmail.com>
2020-05-04 05:52:21 -0400

    articleinfo-gadget.js: Source main gadget at MediaWiki:Gadget-XTools-ArticleInfo.js
    Was created locally to avoid misbehavior on mediawiki, but the enWiki gadget has since been updated

--- userScripts/articleinfo-gadget.js
+++ userScripts/articleinfo-gadget.js
@@ -4,4 +4,9 @@
  * Released under GPL v3 license.
  * Local version of [[mw:XTools/ArticleInfo.js]]
  */
-$(function(){if(mw.config.get("wgArticleId")===0||mw.config.get("wgCurRevisionId")!==mw.config.get("wgRevisionId")||mw.config.get("wgAction")!=="view"){return}var e="<div id='xtools' style='font-size:84%; line-height:1.2em;"+"width:auto;'><span id='xtools_result'>.</span></div>";$(e).insertBefore("#contentSub");var t=window.setInterval(function(){if($("#xtools_result").html()===".&nbsp;&nbsp;"){$("#xtools_result").html("&nbsp;.&nbsp;")}else if($("#xtools_result").html()==="&nbsp;.&nbsp;"){$("#xtools_result").html("&nbsp;&nbsp;.")}else{$("#xtools_result").html(".&nbsp;&nbsp;")}},300);$.get("https://xtools.wmflabs.org"+"/api/page/articleinfo/"+mw.config.get("wgServerName")+"/"+mw.config.get("wgPageName").replace(/["?%&+]/g,escape)+"?format=html"+"&uselang="+mw.config.get("wgUserLanguage")).done(function(e){$("#xtools_result").html(e);clearInterval(t)})});
+
+/**
+ * Source main enWiki gadget: [[MediaWiki:Gadget-XTools-ArticleInfo.js]]
+ */
+
+mw.loader.load('ext.gadget.XTools-ArticleInfo');
