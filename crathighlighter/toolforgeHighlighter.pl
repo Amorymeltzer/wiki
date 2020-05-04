ae94429ae1f34260c179d602762d0720deeddd3f
Amory Meltzer <Amorymeltzer@gmail.com>
2020-05-04 14:58:25 -0400

    toolforge: Duh, need to also remove stdout logging
    Really, don't need the whole cron trace/info thing either.  What I should do is stop using easy_init and define a category for debugging, but I don't think there's much need for it right now

--- crathighlighter/toolforgeHighlighter.pl
+++ crathighlighter/toolforgeHighlighter.pl
@@ -28,14 +28,11 @@ getopts('pc', \%opts);
 
 # The full options are straightforward, but overly verbose when easy mode
 # (and stealth loggers) is succinct and sufficient
-Log::Log4perl->easy_init({ level    => exists $ENV{CRON} ? $TRACE : $INFO,
-			   file     => ">>$ENV{HOME}/logs/tflog.log",
-			   utf8     => 1,
-			   layout   => '%d{yyyy-MM-dd HH:mm:ss} (%p): %m{indent}%n' },
-			 { level    => $TRACE,
-			   file     => 'STDOUT',
-			   layout   => '%m{indent}%n' }
-                        );
+Log::Log4perl->easy_init({ level  =>  $INFO,
+			   file   =>  ">>$ENV{HOME}/logs/tflog.log",
+			   utf8   =>  1,
+			   layout =>  '%d{yyyy-MM-dd HH:mm:ss} (%p): %m{indent}%n'
+			 });
 
 # Check and update repo before doing anything risky
 my $repo = Git::Repository->new();
