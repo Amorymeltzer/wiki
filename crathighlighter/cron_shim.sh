4d501f1ef98b818c5c875c1750f19fadf755ae70
Amory Meltzer <Amorymeltzer@gmail.com>
2020-06-11 20:38:31 -0400

    Quick little shim to let cron easily rerun a command if logs show a failure

---
+++ crathighlighter/cron_shim.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# cron_shim.sh by Amory Meltzer
+# Little shim for cron to grep a file and exit if not found
+# Basically, to make cron rerun a command quickly if the last one failed
+
+file=$1
+search=$2
+
+if [[ ! $(tail -n 1 "$file" | grep "$2") ]]; then
+    exit 1
+fi
