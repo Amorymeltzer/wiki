4b73f3c088367ee0795f4f9c3023c94abf9b800b
Amory Meltzer <Amorymeltzer@gmail.com>
2021-01-30 06:38:16 -0500

    skinPages.js: Expand with other useful queries

--- imports/skinPages.js
+++ imports/skinPages.js
@@ -17,3 +17,33 @@ var api = new Morebits.wiki.api('Get pages', query, function(data){
 	});
 });
 api.post();
+
+/********************************************************/
+
+var skins = [];
+skins = skins.filter(function(page) {
+    return /common.js/i.test(page);
+});
+
+/********************************************************/
+
+var pages = [];
+var ret = [];
+var query2 = {
+	action: 'query',
+	format: 'json',
+	prop: 'info',
+	inprop: '',
+	formatversion: 2
+};
+query2.titles = pages.slice(0, 500).join('|');
+var api2 = new Morebits.wiki.api('Get pages', query2, function(data){
+    data.response.query.pages.forEach(function(page) {
+	page.title && ret.push(page.title);
+    });
+});
+api2.post();
+
+var sorted = ret.sort(function(one, two){
+    return new Date(one.timestamp).getTime() > new Date(two.timestamp).getTime() ? 1 : -1;
+});
