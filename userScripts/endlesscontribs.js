aec5564b70f13635d14a9a5f7fcc79ec1295cc0e
Amory Meltzer <Amorymeltzer@gmail.com>
2021-02-05 17:02:22 -0500

    Copy from [[User:Bradv/endlesscontribs.js]] (as of [[Special:PermaLink/995418220]]) to test post-running function

---
+++ userScripts/endlesscontribs.js
@@ -0,0 +1,49 @@
+// Taken from [[User:Bradv/endlesscontribs.js]] (as of [[Special:PermaLink/995418220]]) to test post-running function
+(function( $, mw ) {
+    'use strict';
+    
+    if (mw.config.get('wgCanonicalSpecialPageName')==='Contributions') {
+        mw.loader.using("mediawiki.util").then(function () {
+            const css = mw.util.addCSS(`
+                body.endlesscontribs .mw-pager-navigation-bar:not(:first-of-type) {
+                    display: none;
+                }
+                #endlesscontribs {
+                    margin: 1em;
+                }
+            `)
+
+            var nexturl = $('a[rel="next"]').attr('href');
+            if (nexturl) {
+                var loading = false;
+
+                const button = $('<span>', {
+                    'id': 'endlesscontribs',
+                    'class': 'mw-ui-button mw-ui-progressive'})
+                .text('Load more')
+                .insertAfter($('.mw-contributions-list'))
+                .click(function () {
+                    $('body').addClass('endlesscontribs');
+                    if (!loading) {
+                        loading = true;
+                        $.get(nexturl, function(data) {
+                            var $html = $(data);
+                            const $ul = $('.mw-contributions-list');
+                            const $newul = $html.find('.mw-contributions-list > li');
+                            $newul.each(function (index, element) {
+                                $ul.append($(element));
+                            })
+
+                            nexturl = $html.find('a[rel="next"]').attr('href');
+
+                            if (!nexturl) {
+                                button.remove();
+                            }
+                            loading = false;
+                        })
+                    }
+                });                
+            }
+        });
+    }
+}(jQuery, mediaWiki ));
