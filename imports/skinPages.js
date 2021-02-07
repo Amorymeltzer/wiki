fb26d57ab1f52176663c19b1a243c80fc5212434
Amory Meltzer <Amorymeltzer@gmail.com>
2021-02-07 07:43:02 -0500

    Get content and parse imported scripts

--- imports/skinPages.js
+++ imports/skinPages.js
@@ -1,20 +1,20 @@
 var query = {
-	action: 'query',
-	format: 'json',
-	list: 'search',
-	formatversion: '2',
-	srsearch: 'intitle:/vector.js|common.js|monobook.js|modern.js|timeless.js|cologneblue.js|minerva.js/ contentmodel:javascript insource:/[ (=+,!]wg[A-Za-z]/',
-	srnamespace: '2',
-	srlimit: 'max',
-	srprop: '',
-	srsort: 'last_edit_desc'
+    action: 'query',
+    format: 'json',
+    list: 'search',
+    formatversion: '2',
+    srsearch: 'intitle:/vector.js|common.js|monobook.js|modern.js|timeless.js|cologneblue.js|minerva.js/ contentmodel:javascript insource:/[ (=+,!]wg[A-Za-z]/',
+    srnamespace: '2',
+    srlimit: 'max',
+    srprop: '',
+    srsort: 'last_edit_desc'
 };
 var res = [];
 var api = new Morebits.wiki.api('Get pages', query, function(data){
-	var search = data.response.query.search;
-	search.forEach(function(title){
-		title.title && res.push(title.title);
-	});
+    var search = data.response.query.search;
+    search.forEach(function(title){
+	title.title && res.push(title.title);
+    });
 });
 api.post();
 
@@ -30,11 +30,11 @@ skins = skins.filter(function(page) {
 var pages = [];
 var ret = [];
 var query2 = {
-	action: 'query',
-	format: 'json',
-	prop: 'info',
-	inprop: '',
-	formatversion: 2
+    action: 'query',
+    format: 'json',
+    prop: 'info',
+    inprop: '',
+    formatversion: 2
 };
 query2.titles = pages.slice(0, 500).join('|');
 var api2 = new Morebits.wiki.api('Get pages', query2, function(data){
@@ -47,3 +47,44 @@ api2.post();
 var sorted = ret.sort(function(one, two){
     return new Date(one.timestamp).getTime() > new Date(two.timestamp).getTime() ? 1 : -1;
 });
+
+/********************************************************/
+
+var actives = [];
+var total = [], ordered = {};
+var query3 = {
+    action: 'query',
+    format: 'json',
+    prop: 'revisions',
+    rvprop: 'content',
+    formatversion: 2
+};
+query3.titles = actives.slice(0, 500).join('|');
+var api3 = new Morebits.wiki.api('Get pages', query3, function(data){
+    var isRE = /importScript\(['"](.+?\.js)['"]\)/g;
+    var mlRE = /mw.loader.load\(['"].*[?&]title=(.+?\.js).*['"]\)/g;
+    data.response.query.pages.forEach(function(page) {
+	var content = page.revisions[0].content;
+	// matchAll returns info about matching group
+	var imports = Array.from(content.matchAll(isRE), m=>m[1]);
+	var loaders = Array.from(content.matchAll(mlRE), m=>m[1]);
+	if (imports.length) {
+	    total = total.concat(imports);
+	}
+	if (loaders.length) {
+	    total = total.concat(loaders);
+	}
+    });
+});
+api3.post();
+
+var count = {};
+total.forEach(function(item) {
+    if (!count[item]) {
+	count[item] = 1;
+    } else {
+	count[item]++;
+    }
+});
+// Object sorted by key values
+ordered = Object.fromEntries(Object.entries(count).sort(([,a],[,b]) => a<b));
