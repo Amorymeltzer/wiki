759c3e6c16b9f96f479b500bc84b3523f41b3d95
Amory Meltzer <Amorymeltzer@gmail.com>
2022-12-08 09:27:18 -0500

    AmoryBot::CratHighlighter: Remove excess commented-out code
    Not used yet/ever, don't want to update in two places as I maintain

--- crathighlighter/lib/AmoryBot/CratHighlighter/GitUtils.pm
+++ crathighlighter/lib/AmoryBot/CratHighlighter/GitUtils.pm
@@ -74,51 +74,13 @@ sub gitSHA {
 
 Amory Meltzer, C<< <Amorymeltzer at gmail.com> >>
 
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-amorybot-crathighlighter-gitutils at rt.cpan.org>, or through
-the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=AmoryBot-CratHighlighter-GitUtils>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc AmoryBot::CratHighlighter::GitUtils
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=AmoryBot-CratHighlighter-GitUtils>
-
-=item * CPAN Ratings
-
-L<https://cpanratings.perl.org/d/AmoryBot-CratHighlighter-GitUtils>
-
-=item * Search CPAN
-
-L<https://metacpan.org/release/AmoryBot-CratHighlighter-GitUtils>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
 =head1 LICENSE AND COPYRIGHT
 
 This software is Copyright (c) 2022 by Amory Meltzer.
 
 This is free software, licensed under:
 
-  The Artistic License 2.0 (GPL Compatible)
+  The WTFPL
 
 
 =cut
