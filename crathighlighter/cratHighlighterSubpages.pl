Amorymeltzer/wiki

commit 1200e39a55ef2226ad302c045789833b90c0b0ec

cratHighlighterSubpages.pl: use  for UA instead of hardcoding it
Don't know why I wanted to rename it, fine as is

crathighlighter/cratHighlighterSubpages.pl
@@ -219,8 +219,7 @@ sub mwLogin {
 			     on_error => \&dieNice,
 			     use_http_get => '1' # use GET where appropriate
 			    });
-  # TODO Rename to just cratHighlighter
-  $mw->{ua}->agent('cratHighlighterSubpages.pl ('.$mw->{ua}->agent.')');
+  $mw->{ua}->agent($PROGRAM_NAME.' ('.$mw->{ua}->agent.')');
   $mw->login({lgname => $username, lgpassword => $password});
 
   return $mw;

commit 51452a5c97cabe907205893dcc23dcbc40898e2b

cratHighlighterSubpages.pl: Fix UA bug introduced in 1200e39
Using `$PROGRAM_NAME` didn't work as expected, it wasn't interpolated there so just sent on its way through the stack!

crathighlighter/cratHighlighterSubpages.pl
@@ -211,7 +211,7 @@ sub mwLogin {
 			     on_error => \&dieNice,
 			     use_http_get => '1' # use GET where appropriate
 			    });
-  $mw->{ua}->agent($PROGRAM_NAME.' ('.$mw->{ua}->agent.')');
+  $mw->{ua}->agent("$PROGRAM_NAME (".$mw->{ua}->agent.')');
   $mw->login({lgname => $username, lgpassword => $password});
 
   return $mw;