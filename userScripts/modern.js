4c6aa27f7d4a5260021090148af9f85e0fa80ede
Amory Meltzer <Amorymeltzer@gmail.com>
2024-08-23 07:29:36 -0400

    modern: C4

--- userScripts/modern.js
+++ userScripts/modern.js
@@ -197,7 +197,7 @@ if (cfg.wgCanonicalNamespace === 'Special') {
 	  This will eventually devolve to Mainspace or not
 	*/
 	/* Not Special */
-	mw.loader.load('//en.wikipedia.org/w/index.php?title=User:Ale_jrb/Scripts/csdhelper.js&oldid=1139126287&action=raw&ctype=text/javascript'); // [[User:Ale jrb/Scripts]], [[User:Ale jrb/Scripts/csdhelper.js]]
+	mw.loader.load('//en.wikipedia.org/w/index.php?title=User:Ale_jrb/Scripts/csdhelper.js&oldid=1241828968&action=raw&ctype=text/javascript'); // [[User:Ale jrb/Scripts]], [[User:Ale jrb/Scripts/csdhelper.js]]
 	window.notifyByDefaultDec = true; // default is true
 	// window.notifyByDefaultDel = true; //default is false
 	window.notifyByDefaultPrd = true; // default is true
